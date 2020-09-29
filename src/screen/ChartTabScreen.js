0f287b7b2219bfd16b329dbe8c51adee09d4ee82
Vaibhaw Anand <vaibhaw@redappletech.com>
2020-09-30 01:23:35 +0530

    Chart Added

--- src/screen/ChartTabScreen.js
+++ src/screen/ChartTabScreen.js
@@ -9,13 +9,16 @@ import {
   Text,
 } from 'react-native';
 
-import { ProgressCircle } from 'react-native-svg-charts'
+import { AreaChart, Grid, ProgressCircle } from 'react-native-svg-charts';
+import * as shape from 'd3-shape'
+
 const initialLayout = { width: Dimensions.get('window').width, height: Dimensions.get('window').height };
 
 export default class ChatTabScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      areaChart: [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80],
       LineChart: {
         labels: ["January", "February", "March", "April", "May", "June"],
         datasets: [
@@ -40,10 +43,22 @@ export default class ChatTabScreen extends React.Component {
         <StatusBar barStyle="light-content" />
         <SafeAreaView />
         <View style={styles.container}>
-          <View>
+          <View style={{flex:1}}>
             <Text>Progress Chart</Text>
             <ProgressCircle style={{ height: 200 }} progress={0.7} progressColor={'rgb(134, 65, 244)'} />
-            </View>
+          </View>
+          <View style={{flex: 1,}}>
+            <Text>Area Chart</Text>
+            <AreaChart
+                style={{ height: 200, width: initialLayout.width*0.9 }}
+                data={this.state.areaChart}
+                contentInset={{ top: 30, bottom: 30 }}
+                curve={shape.curveNatural}
+                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+            >
+                <Grid />
+            </AreaChart>
+          </View>
         </View>
       </>
     )
@@ -53,6 +68,7 @@ export default class ChatTabScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#ccc',
     justifyContent: 'center',
     alignItems: 'center',
